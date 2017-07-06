b94fbfa13235616b51bebd77869a0a7a3d7f8a93
jjhavstad <jonathan.havstad@outlook.com>
2017-07-06 09:19:20 -0400

    Adding Facebook Login functionality and removing uneeded tests

---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/ui/authentication/FacebookLoginResponse.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.data.appcms.ui.authentication;
+
+/**
+ * Created by viewlift on 7/6/17.
+ */
+
+public class FacebookLoginResponse {
+}
---
+++ AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSFacebookLoginCall.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.network.rest;
+
+/**
+ * Created by viewlift on 7/6/17.
+ */
+
+public class AppCMSFacebookLoginCall {
+}
