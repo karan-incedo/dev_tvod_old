6bdaea71e5589c7a569b08c874444795750d63c7
Chornge <christian@viewlift.com>
2017-07-12 16:25:14 -0400

    Added more subscription models.

--- AppCMS/src/main/java/com/viewlift/models/data/appcms/subscriptions/AppCMSSubscriptionResult.java
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/subscriptions/AppCMSSubscriptionResult.java
@@ -1,27 +1,9 @@
-package com.viewlift.models.data.appcms.payments;
+package com.viewlift.models.data.appcms.subscriptions;
 
 /*
  * Created by Viewlift on 7/12/17.
  */
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 public class AppCMSSubscriptionResult {
-
-    @SerializedName("a")
-    @Expose
-    private String a;
-
-    @SerializedName("b")
-    @Expose
-    private String b;
-
-    @SerializedName("c")
-    @Expose
-    private String c;
-
-    @SerializedName("d")
-    @Expose
-    private String d;
+    //
 }
