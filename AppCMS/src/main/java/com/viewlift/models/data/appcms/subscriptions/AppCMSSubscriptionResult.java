351575bf235fd4f1f18898b5a93f0566a799aa88
Chornge <christian@viewlift.com>
2017-07-12 16:22:33 -0400

    Updated versionCode to 11, versionName to 1.0.21 and hostName to 'http://www.hoichoi.tv'

---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/subscriptions/AppCMSSubscriptionResult.java
@@ -0,0 +1,27 @@
+package com.viewlift.models.data.appcms.payments;
+
+/*
+ * Created by Viewlift on 7/12/17.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AppCMSSubscriptionResult {
+
+    @SerializedName("a")
+    @Expose
+    private String a;
+
+    @SerializedName("b")
+    @Expose
+    private String b;
+
+    @SerializedName("c")
+    @Expose
+    private String c;
+
+    @SerializedName("d")
+    @Expose
+    private String d;
+}
