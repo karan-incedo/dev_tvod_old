bee2e5d2ab41f313c532571983503edf8c736681
Chornge <christian@viewlift.com>
2017-07-24 18:04:21 -0400

    Added google sign up/login functionlaty.

---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/ui/authentication/GoogleLoginResponse.java
@@ -0,0 +1,13 @@
+package com.viewlift.models.data.appcms.ui.authentication;
+
+/*
+ * Created by Viewlift on 7/24/17.
+ */
+
+import com.vimeo.stag.UseStag;
+
+@UseStag
+public class GoogleLoginResponse {
+
+    //
+}
