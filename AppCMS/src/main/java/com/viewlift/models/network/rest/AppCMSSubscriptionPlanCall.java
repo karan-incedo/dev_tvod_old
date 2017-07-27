e36c08d9f67c7940efffbf3aae975308de1e97c6
jjhavstad <jonathan.havstad@outlook.com>
2017-07-27 16:29:54 -0400

    Added null checks for call to retrieve subscription data.

--- AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSSubscriptionPlanCall.java
+++ AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSSubscriptionPlanCall.java
@@ -67,12 +67,17 @@ public class AppCMSSubscriptionPlanCall {
                 appCMSSubscriptionPlanRest.getSubscribedPlan(url).enqueue(new Callback<AppCMSSubscriptionPlanResult>() {
                     @Override
                     public void onResponse(Call<AppCMSSubscriptionPlanResult> call, Response<AppCMSSubscriptionPlanResult> response) {
-                        Observable.just(response.body()).subscribe(resultAction1);
+                        if (response != null) {
+                            Observable.just(response.body()).subscribe(resultAction1);
+                        } else {
+                            Observable.just((AppCMSSubscriptionPlanResult) null).subscribe(resultAction1);
+                        }
                     }
 
                     @Override
                     public void onFailure(Call<AppCMSSubscriptionPlanResult> call, Throwable t) {
                         Log.e(TAG, "onFailure: " + t.getMessage());
+                        Observable.just((AppCMSSubscriptionPlanResult) null).subscribe(resultAction1);
                     }
                 });
                 break;
