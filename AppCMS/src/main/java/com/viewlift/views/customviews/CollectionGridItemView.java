b232060889be6bd2d292f9b6a299a584552166db
jjhavstad <jonathan.havstad@outlook.com>
2017-08-14 19:27:51 -0400

    Applied fix to keep aspect ratio for images that are scaled up.

--- AppCMS/src/main/java/com/viewlift/views/customviews/CollectionGridItemView.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/CollectionGridItemView.java
@@ -237,7 +237,6 @@ public class CollectionGridItemView extends BaseView {
                         try {
                             final int imageWidth = deviceWidth;
                             final int imageHeight = childViewHeight;
-                            final ImageView imageView = (ImageView) view;
                             StringBuilder imageMetaData = new StringBuilder();
                             imageMetaData.append(imageUrl);
                             imageMetaData.append(System.currentTimeMillis() / 60000);
@@ -254,27 +253,28 @@ public class CollectionGridItemView extends BaseView {
                                         protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
                                             int width = toTransform.getWidth();
                                             int height = toTransform.getHeight();
-                                            Bitmap sourceWithGradient = null;
 
                                             boolean scaleImageUp = false;
+
+                                            Bitmap sourceWithGradient;
                                             if (width < imageWidth &&
                                                     height < imageHeight) {
+                                                scaleImageUp = true;
+                                                float widthToHeightRatio =
+                                                        (float) width / (float) height;
                                                 sourceWithGradient =
                                                         Bitmap.createScaledBitmap(toTransform,
-                                                                imageWidth,
+                                                                (int) (imageHeight * widthToHeightRatio),
                                                                 imageHeight,
                                                                 false);
-                                                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                                                scaleImageUp = true;
                                             } else {
-                                                sourceWithGradient = Bitmap.createBitmap(width,
-                                                        height,
-                                                        Bitmap.Config.ARGB_8888);
-                                                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                                                sourceWithGradient =
+                                                        Bitmap.createBitmap(width,
+                                                                height,
+                                                                Bitmap.Config.ARGB_8888);
                                             }
 
                                             Canvas canvas = new Canvas(sourceWithGradient);
-
                                             if (!scaleImageUp) {
                                                 canvas.drawBitmap(toTransform, 0, 0, null);
                                             }
