9530bb2c57e8aea46985f4b837d8d6317ca4ac70
Chornge <christian@viewlift.com>
2017-08-28 19:20:46 -0400

    ƒixed #SVFA-1819.

--- AppCMS/src/main/java/com/viewlift/views/fragments/AppCMSChangePasswordFragment.java
+++ AppCMS/src/main/java/com/viewlift/views/fragments/AppCMSChangePasswordFragment.java
@@ -2,17 +2,20 @@ package com.viewlift.views.fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.viewlift.AppCMSApplication;
 import com.viewlift.R;
 import com.viewlift.presenters.AppCMSPresenter;
+import com.viewlift.views.customviews.AsteriskPasswordTransformation;
 import com.viewlift.views.customviews.ViewCreator;
 
 import butterknife.BindView;
@@ -26,14 +29,23 @@ public class AppCMSChangePasswordFragment extends android.support.v4.app.Fragmen
     @BindView(R.id.app_cms_change_password_page_title)
     TextView changePasswordPageTitle;
 
-    @BindView(R.id.app_cms_change_old_password_text_input)
-    EditText oldPasswordInput;
+    @BindView(R.id.app_cms_old_password_container)
+    TextInputLayout oldPasswordInputLayout;
 
-    @BindView(R.id.app_cms_change_new_password_text_input)
-    EditText newPasswordInput;
+    @BindView(R.id.app_cms_old_password_text_input)
+    TextInputEditText oldPasswordInput;
+
+    @BindView(R.id.app_cms_new_password_container)
+    TextInputLayout newPasswordInputLayout;
+
+    @BindView(R.id.app_cms_new_password_text_input)
+    TextInputEditText newPasswordInput;
+
+    @BindView(R.id.app_cms_confirm_password_container)
+    TextInputLayout confirmPasswordInputLayout;
 
     @BindView(R.id.app_cms_confirm_password_text_input)
-    EditText confirmPasswordInput;
+    TextInputEditText confirmPasswordInput;
 
     @BindView(R.id.app_cms_change_password_button)
     Button confirmPasswordButton;
@@ -63,6 +75,20 @@ public class AppCMSChangePasswordFragment extends android.support.v4.app.Fragmen
         changePasswordPageTitle.setTextColor(Color.parseColor(appCMSPresenter.getAppCMSMain()
                 .getBrand().getGeneral().getTextColor()));
 
+//        oldPasswordInputLayout.addView(view);
+//        newPasswordInputLayout.addView(view);
+//        confirmPasswordInputLayout.addView(view);
+
+        oldPasswordInputLayout.setPasswordVisibilityToggleEnabled(true);
+        newPasswordInputLayout.setPasswordVisibilityToggleEnabled(true);
+        confirmPasswordInputLayout.setPasswordVisibilityToggleEnabled(true);
+
+//        oldPasswordInputLayout.listener
+
+        oldPasswordInput.setTransformationMethod(new AsteriskPasswordTransformation());
+        newPasswordInput.setTransformationMethod(new AsteriskPasswordTransformation());
+        confirmPasswordInput.setTransformationMethod(new AsteriskPasswordTransformation());
+
         confirmPasswordButton.setOnClickListener(v -> {
             String oldPassword = oldPasswordInput.getText().toString().trim();
             String newPassword = newPasswordInput.getText().toString().trim();
@@ -71,7 +97,8 @@ public class AppCMSChangePasswordFragment extends android.support.v4.app.Fragmen
             appCMSPresenter.updateUserPassword(oldPassword, newPassword, confirmPassword);
         });
 
-        confirmPasswordButton.setTextColor(0xff000000 + (int) ViewCreator.adjustColor1(textColor, buttonColor));
+        confirmPasswordButton.setTextColor(0xff000000 + (int) ViewCreator.adjustColor1(textColor,
+                buttonColor));
         confirmPasswordButton.setBackgroundColor(buttonColor);
         setBgColor(bgColor);
 
