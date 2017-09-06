5627e7544497201a91fe274006a2e3d4a1575f15
jjhavstad <jonathan.havstad@outlook.com>
2017-09-06 09:55:09 -0400

    Removed uneeded comment.

--- AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
+++ AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
@@ -4134,7 +4134,6 @@ public class AppCMSPresenter {
     public boolean setLoggedInUserName(Context context, String userName) {
         if (context != null) {
             SharedPreferences sharedPrefs = context.getSharedPreferences(USER_NAME_SHARED_PREF_NAME, 0);
-//            return sharedPrefs.edit().putString(USER_NAME_SHARED_PREF_NAME, userName).commit();
             return sharedPrefs.edit().putString(USER_NAME_SHARED_PREF_NAME, userName).commit();
         }
         return false;
