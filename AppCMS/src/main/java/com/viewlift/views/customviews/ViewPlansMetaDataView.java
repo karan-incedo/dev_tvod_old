367303c5f3e47f4808562af1980d3318b030f2ad
Chornge <christian@viewlift.com>
2017-09-14 19:43:51 -0400

    View plans meta data view now shows the correct number of lines & shows it in the correct format.

--- AppCMS/src/main/java/com/viewlift/views/customviews/ViewPlansMetaDataView.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/ViewPlansMetaDataView.java
@@ -3,9 +3,8 @@ package com.viewlift.views.customviews;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -24,15 +23,12 @@ import java.util.List;
 import java.util.Map;
 
 @SuppressLint("ViewConstructor")
+@SuppressWarnings("FieldCanBeLocal, unused")
 public class ViewPlansMetaDataView extends LinearLayout {
-
-    private static final String TAG = "ViewPlansMetaDataTAG_";
 
     private static int viewCreationPlanDetailsIndex = 0;
 
     private final Component component;
-
-    @SuppressWarnings("FieldCanBeLocal, unused")
     private final Layout layout;
 
     private final ViewCreator viewCreator;
@@ -104,86 +100,36 @@ public class ViewPlansMetaDataView extends LinearLayout {
         planLayout.setLayoutParams(layoutParams);
         planLayout.setOrientation(LinearLayout.HORIZONTAL);
 
+        TextView featureDetailText = getCommaSeparatedFeatureDetail(featureDetails);
+
+        planLayout.addView(featureDetailText);
+
+        addView(planLayout);
+    }
+
+    @NonNull
+    private TextView getCommaSeparatedFeatureDetail(List<FeatureDetail> featureDetails) {
+        TextView featureDetailText = new TextView(getContext());
+        featureDetailText.setMaxLines(2);
+        featureDetailText.setEllipsize(TextUtils.TruncateAt.END);
+        featureDetailText.setTextColor(Color.parseColor(appCMSPresenter.getAppCMSMain()
+                .getBrand().getCta().getPrimary().getTextColor()));
+
         for (int featureDetailsIndex = 0; featureDetailsIndex < featureDetails.size();
              featureDetailsIndex++) {
 
             if (!TextUtils.isEmpty(featureDetails.get(featureDetailsIndex).getValueType())) {
                 FeatureDetail featureDetail = featureDetails.get(featureDetailsIndex);
-                int componentIndex;
+                if (!TextUtils.isEmpty(featureDetail.getValue()) &&
+                        "true".equalsIgnoreCase(featureDetail.getValue())) {
+                    featureDetailText.append(featureDetail.getTextToDisplay());
 
-                if (component.getComponents() != null) {
-                    for (componentIndex = 0; componentIndex < component.getComponents().size();
-                         componentIndex++) {
-                        Component subComponent = component.getComponents().get(componentIndex);
-                        planLayout.addView(addChildComponent(subComponent, featureDetail));
+                    if (featureDetailsIndex < featureDetails.size() - 1) {
+                        featureDetailText.append(", ");
                     }
                 }
-
-                addCommaAfterEachFeature(featureDetails, planLayout, featureDetailsIndex);
             }
         }
-
-        addView(planLayout);
-    }
-
-    private View addChildComponent(Component subComponent, FeatureDetail featureDetail) {
-        ViewCreator.ComponentViewResult componentViewResult = viewCreator.getComponentViewResult();
-
-        viewCreator.createComponentView(getContext(),
-                subComponent,
-                subComponent.getLayout(),
-                moduleAPI,
-                null,
-                moduleSettings,
-                jsonValueKeyMap,
-                appCMSPresenter,
-                false,
-                "");
-
-        View componentView = componentViewResult.componentView;
-        if (componentView != null) {
-            AppCMSUIKeyType componentKeyType = jsonValueKeyMap.get(subComponent.getKey());
-
-            if (componentKeyType == null) {
-                componentKeyType = AppCMSUIKeyType.PAGE_EMPTY_KEY;
-            }
-
-            switch (componentKeyType) {
-                case PAGE_PLANMETADATATITLE_KEY:
-                    if (componentView instanceof TextView) {
-                        componentView.setLayoutParams(new LinearLayout.LayoutParams(
-                                LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                        ((TextView) componentView).setText(featureDetail.getTextToDisplay());
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-
-            Log.d(TAG, "Created child component: " +
-                    planDetailsIndex +
-                    " - " +
-                    featureDetail.getTextToDisplay() +
-                    " - " +
-                    subComponent.getKey() +
-                    " - " +
-                    componentView.getClass().getName());
-        }
-
-        return componentView;
-    }
-
-    private void addCommaAfterEachFeature(List<FeatureDetail> featureDetails, LinearLayout planLayout,
-                                          int featureDetailsIndex) {
-        if (featureDetailsIndex < featureDetails.size() - 1) {
-            TextView commaView = new TextView(getContext());
-            commaView.setText(", ");
-            commaView.setEllipsize(TextUtils.TruncateAt.END);
-            commaView.setHorizontallyScrolling(false);
-            commaView.setTextColor(Color.parseColor(appCMSPresenter.getAppCMSMain()
-                    .getBrand().getCta().getPrimary().getTextColor()));
-            planLayout.addView(commaView);
-        }
+        return featureDetailText;
     }
 }
