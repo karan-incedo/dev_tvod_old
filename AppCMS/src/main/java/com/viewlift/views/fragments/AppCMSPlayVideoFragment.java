617ee7fbd7bfb556a342833a7b9ebead49916526
jjhavstad <jonathan.havstad@outlook.com>
2017-09-22 16:02:17 -0400

    Applied additional fix to only send update watched time and Beacon pings if the time elapsed is greater than 0.

--- AppCMS/src/main/java/com/viewlift/views/fragments/AppCMSPlayVideoFragment.java
+++ AppCMS/src/main/java/com/viewlift/views/fragments/AppCMSPlayVideoFragment.java
@@ -1224,6 +1224,7 @@ public class AppCMSPlayVideoFragment extends Fragment
 
                         long currentTime = videoPlayerView.getCurrentPosition() / 1000;
                         if (appCMSPresenter != null && videoPlayerView != null
+                                && 30 <= (videoPlayerView.getCurrentPosition() / 1000)
                                 && playbackState == ExoPlayer.STATE_READY && currentTime % 30 == 0) { // For not to sent PIN in PAUSE mode
 
                             Log.d(TAG, "Beacon Message Request position: " + currentTime);
