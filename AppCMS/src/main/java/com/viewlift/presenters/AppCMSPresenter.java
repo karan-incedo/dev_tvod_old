56238078aabd0b011effbeff91b2e4efc1f70eb6
sandeep-singh-incedo <sandeep.singh@incedoinc.com>
2017-09-25 16:37:15 +0530

    Change of SDCradLocaction for downloaded files for encryption.

--- AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
+++ AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
@@ -8613,7 +8613,7 @@ public class AppCMSPresenter {
     public String getSDCardPath(Context context) {
         File baseSDCardDir;
         String[] dirs = getStorageDirectories(context);
-        baseSDCardDir = new File(dirs[0] + File.separator + appCMSMain.getDomainName());
+        baseSDCardDir = new File(dirs[0]);
 
         return baseSDCardDir.getAbsolutePath();
     }
@@ -8631,7 +8631,8 @@ public class AppCMSPresenter {
                 for (File file : externalDirs) {
                     String path = null;
                     try {
-                        path = file.getPath().split("/Android")[0];
+                       // path = file.getPath().split("/Android")[0];
+                        path = file.getAbsolutePath();
                     } catch (Exception e) {
                         Log.e(TAG, "Error getting storage directories for downloads: " + e.getMessage());
                         path = null;
