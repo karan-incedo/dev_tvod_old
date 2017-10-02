92376af8f959fd54cb0b03086739240ed11e1ea0
jjhavstad <jonathan.havstad@outlook.com>
2017-10-02 11:29:22 -0400

    Uncommented add video listener in video player.

--- AppCMS/src/main/java/com/viewlift/views/customviews/VideoPlayerView.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/VideoPlayerView.java
@@ -269,7 +269,7 @@ public class VideoPlayerView extends FrameLayout implements Player.EventListener
                 new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
         DefaultTrackSelector trackSelector =
                 new DefaultTrackSelector(videoTrackSelectionFactory);
-        
+
         trackSelector.setTunnelingAudioSessionId(C.generateAudioSessionIdV21(getContext()));
 
         player = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
@@ -280,7 +280,7 @@ public class VideoPlayerView extends FrameLayout implements Player.EventListener
                 onPlayerControlsStateChanged.call(visibility);
             }
         });
-//        player.addVideoListener(this);
+        player.addVideoListener(this);
 
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         audioManager.requestAudioFocus(focusChange -> Log.i(TAG, "Audio focus has changed: " + focusChange),
