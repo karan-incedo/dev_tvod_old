ed7aac5254c378b40e53688eae27e28374770566
Chornge <christian@viewlift.com>
2017-10-09 23:03:44 -0400

    Applied ƒix for #SVFA-2086.

--- AppCMS/src/main/java/com/viewlift/views/customviews/ViewCreator.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/ViewCreator.java
@@ -1636,9 +1636,15 @@ public class ViewCreator {
                 } else {
                     if (!TextUtils.isEmpty(appCMSPresenter.getAppCMSMain().getBrand().getGeneral()
                             .getBlockTitleColor())) {
+//                        componentViewResult.componentView.setBackgroundColor(Color.parseColor(
+//                                getColor(context, appCMSPresenter.getAppCMSMain().getBrand()
+//                                        .getGeneral().getBlockTitleColor())));
                         componentViewResult.componentView.setBackgroundColor(Color.parseColor(
                                 getColor(context, appCMSPresenter.getAppCMSMain().getBrand()
-                                        .getGeneral().getBlockTitleColor())));
+                                        .getGeneral().getBackgroundColor())));
+                        applyBorderToComponent(context, componentViewResult.componentView, component,
+                                Color.parseColor(appCMSPresenter.getAppCMSMain().getBrand()
+                                        .getGeneral().getBlockTitleColor()));
                     } else {
                         applyBorderToComponent(context, componentViewResult.componentView, component, -1);
                     }
