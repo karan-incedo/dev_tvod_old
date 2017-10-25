a84323bc1b36b15492d9e24001db88d07b9b774c
jjhavstad <jonathan.havstad@outlook.com>
2017-10-25 14:29:23 -0400

    Fixed crash for SVFA-2419.

--- AppCMS/src/main/java/com/viewlift/views/activity/AppCMSPlayVideoActivity.java
+++ AppCMS/src/main/java/com/viewlift/views/activity/AppCMSPlayVideoActivity.java
@@ -135,9 +135,9 @@ public class AppCMSPlayVideoActivity extends AppCompatActivity implements
                     if (((binder != null &&
                             binder.getContentData() != null &&
                             binder.getContentData().getGist() != null &&
-                            binder.getContentData().getGist().getDownloadStatus() != null &&
-                            binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_COMPLETED &&
-                            binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_SUCCESSFUL) ||
+                            (binder.getContentData().getGist().getDownloadStatus() != null &&
+                                    binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_COMPLETED &&
+                                    binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_SUCCESSFUL) ||
                             binder.getContentData().getGist().getDownloadStatus() == null) &&
                             (activeNetwork == null ||
                             !activeNetwork.isConnectedOrConnecting())) {
@@ -149,10 +149,11 @@ public class AppCMSPlayVideoActivity extends AppCompatActivity implements
                 } catch (Exception e) {
                     if ((binder != null &&
                             binder.getContentData() != null &&
-                            binder.getContentData().getGist() != null &&binder.getContentData().getGist().getDownloadStatus() != null &&
+                            binder.getContentData().getGist() != null &&
+                            (binder.getContentData().getGist().getDownloadStatus() != null &&
                             binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_COMPLETED &&
                             binder.getContentData().getGist().getDownloadStatus() != DownloadStatus.STATUS_SUCCESSFUL) ||
-                            binder.getContentData().getGist().getDownloadStatus() == null) {
+                            binder.getContentData().getGist().getDownloadStatus() == null)) {
                         appCMSPresenter.showDialog(AppCMSPresenter.DialogType.NETWORK,
                                 appCMSPresenter.getNetworkConnectedVideoPlayerErrorMsg(),
                                 false, () -> closePlayer(),
