e11450c87743ca39d18705f1bd1516fd4f6fc974
jjhavstad <jonathan.havstad@outlook.com>
2017-10-26 15:04:15 -0400

    Applied fix for SVFA-2427.

--- AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
+++ AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
@@ -3193,94 +3193,93 @@ public class AppCMSPresenter {
                               Action1<UserVideoDownloadStatus> resultAction1) {
         refreshVideoData(contentDatum, updateContentDatum -> {
             if (updateContentDatum != null &&
-                    updateContentDatum.getGist().getId() != null) {
-                getAppCMSSignedURL(updateContentDatum.getGist().getId(), appCMSSignedURLResult -> {
-                    currentActivity.runOnUiThread(() -> {
-                        try {
+                updateContentDatum.getGist().getId() != null) {
 
-                            long enqueueId;
+                currentActivity.runOnUiThread(() -> {
+                    try {
 
-                            if (updateContentDatum.getStreamingInfo() == null) { // This will handle the case if we get video streaming info null at Video detail page.
+                        long enqueueId;
 
-                                String url = getStreamingInfoURL(updateContentDatum.getGist().getId());
+                        if (updateContentDatum.getStreamingInfo() == null) { // This will handle the case if we get video streaming info null at Video detail page.
 
-                                GetAppCMSStreamingInfoAsyncTask.Params param = new GetAppCMSStreamingInfoAsyncTask.Params.Builder().url(url).build();
+                            String url = getStreamingInfoURL(updateContentDatum.getGist().getId());
 
-                                new GetAppCMSStreamingInfoAsyncTask(appCMSStreamingInfoCall, appCMSStreamingInfo -> {
-                                    if (appCMSStreamingInfo != null) {
-                                        updateContentDatum.setStreamingInfo(appCMSStreamingInfo.getStreamingInfo());
-                                    }
-                                }).execute(param);
+                            GetAppCMSStreamingInfoAsyncTask.Params param = new GetAppCMSStreamingInfoAsyncTask.Params.Builder().url(url).build();
 
-                                showDialog(DialogType.STREAMING_INFO_MISSING, null, false, null, null);
-                                return;
-                            }
+                            new GetAppCMSStreamingInfoAsyncTask(appCMSStreamingInfoCall, appCMSStreamingInfo -> {
+                                if (appCMSStreamingInfo != null) {
+                                    updateContentDatum.setStreamingInfo(appCMSStreamingInfo.getStreamingInfo());
+                                }
+                            }).execute(param);
 
-                            long ccEnqueueId = 0L;
-                            if (updateContentDatum.getContentDetails() != null &&
-                                    updateContentDatum.getContentDetails().getClosedCaptions() != null &&
-                                    !updateContentDatum.getContentDetails().getClosedCaptions().isEmpty() &&
-                                    updateContentDatum.getContentDetails().getClosedCaptions().get(0).getUrl() != null) {
-                                ccEnqueueId = downloadVideoSubtitles(updateContentDatum.getContentDetails()
-                                        .getClosedCaptions().get(0).getUrl(), updateContentDatum.getGist().getId());
-                            }
-
-                            cancelDownloadIconTimerTask();
-
-                            String downloadURL;
-
-
-                            int bitrate = updateContentDatum.getStreamingInfo().getVideoAssets().getMpeg().get(0).getBitrate();
-
-
-                            downloadURL = getDownloadURL(updateContentDatum);
-
-                            DownloadManager.Request downloadRequest = new DownloadManager.Request(Uri.parse(downloadURL.replace(" ", "%20")))
-                                    .setTitle(updateContentDatum.getGist().getTitle())
-                                    .setDescription(updateContentDatum.getGist().getDescription())
-                                    .setAllowedOverRoaming(false)
-                                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                                    .setVisibleInDownloadsUi(false)
-                                    .setShowRunningNotification(true);
-
-                            if (getUserDownloadLocationPref()) {
-                                downloadRequest.setDestinationUri(Uri.fromFile(new File(getSDCardPath(currentActivity, Environment.DIRECTORY_DOWNLOADS),
-                                        updateContentDatum.getGist().getId() + MEDIA_SURFIX_MP4)));
-                            } else {
-                                downloadRequest.setDestinationInExternalFilesDir(currentActivity, Environment.DIRECTORY_DOWNLOADS,
-                                        updateContentDatum.getGist().getId() + MEDIA_SURFIX_MP4);
-                            }
-
-                            enqueueId = downloadManager.enqueue(downloadRequest);
-
-                            long thumbEnqueueId = downloadVideoImage(updateContentDatum.getGist().getVideoImageUrl(),
-                                    updateContentDatum.getGist().getId());
-                            long posterEnqueueId = downloadPosterImage(updateContentDatum.getGist().getPosterImageUrl(),
-                                    updateContentDatum.getGist().getId());
-
-                            /*
-                             * Inserting data in realm data object
-                             */
-                            createLocalEntry(
-                                    enqueueId,
-                                    thumbEnqueueId,
-                                    posterEnqueueId,
-                                    ccEnqueueId,
-                                    updateContentDatum,
-                                    downloadURL);
-
-                            showToast(
-                                    currentActivity.getString(R.string.app_cms_download_started_message,
-                                            updateContentDatum.getGist().getTitle()), Toast.LENGTH_LONG);
-
-                        } catch (Exception e) {
-                            //Log.e(TAG, e.getMessage());
-                            showDialog(DialogType.DOWNLOAD_INCOMPLETE, e.getMessage(), false, null, null);
-                        } finally {
-                            appCMSUserDownloadVideoStatusCall.call(updateContentDatum.getGist().getId(), this,
-                                    resultAction1, getLoggedInUser());
+                            showDialog(DialogType.STREAMING_INFO_MISSING, null, false, null, null);
+                            return;
                         }
-                    });
+
+                        long ccEnqueueId = 0L;
+                        if (updateContentDatum.getContentDetails() != null &&
+                                updateContentDatum.getContentDetails().getClosedCaptions() != null &&
+                                !updateContentDatum.getContentDetails().getClosedCaptions().isEmpty() &&
+                                updateContentDatum.getContentDetails().getClosedCaptions().get(0).getUrl() != null) {
+                            ccEnqueueId = downloadVideoSubtitles(updateContentDatum.getContentDetails()
+                                    .getClosedCaptions().get(0).getUrl(), updateContentDatum.getGist().getId());
+                        }
+
+                        cancelDownloadIconTimerTask();
+
+                        String downloadURL;
+
+
+                        int bitrate = updateContentDatum.getStreamingInfo().getVideoAssets().getMpeg().get(0).getBitrate();
+
+
+                        downloadURL = getDownloadURL(updateContentDatum);
+
+                        DownloadManager.Request downloadRequest = new DownloadManager.Request(Uri.parse(downloadURL.replace(" ", "%20")))
+                                .setTitle(updateContentDatum.getGist().getTitle())
+                                .setDescription(updateContentDatum.getGist().getDescription())
+                                .setAllowedOverRoaming(false)
+                                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                                .setVisibleInDownloadsUi(false)
+                                .setShowRunningNotification(true);
+
+                        if (getUserDownloadLocationPref()) {
+                            downloadRequest.setDestinationUri(Uri.fromFile(new File(getSDCardPath(currentActivity, Environment.DIRECTORY_DOWNLOADS),
+                                    updateContentDatum.getGist().getId() + MEDIA_SURFIX_MP4)));
+                        } else {
+                            downloadRequest.setDestinationInExternalFilesDir(currentActivity, Environment.DIRECTORY_DOWNLOADS,
+                                    updateContentDatum.getGist().getId() + MEDIA_SURFIX_MP4);
+                        }
+
+                        enqueueId = downloadManager.enqueue(downloadRequest);
+
+                        long thumbEnqueueId = downloadVideoImage(updateContentDatum.getGist().getVideoImageUrl(),
+                                updateContentDatum.getGist().getId());
+                        long posterEnqueueId = downloadPosterImage(updateContentDatum.getGist().getPosterImageUrl(),
+                                updateContentDatum.getGist().getId());
+
+                        /*
+                         * Inserting data in realm data object
+                         */
+                        createLocalEntry(
+                                enqueueId,
+                                thumbEnqueueId,
+                                posterEnqueueId,
+                                ccEnqueueId,
+                                updateContentDatum,
+                                downloadURL);
+
+                        showToast(
+                                currentActivity.getString(R.string.app_cms_download_started_message,
+                                        updateContentDatum.getGist().getTitle()), Toast.LENGTH_LONG);
+
+                    } catch (Exception e) {
+                        //Log.e(TAG, e.getMessage());
+                        showDialog(DialogType.DOWNLOAD_INCOMPLETE, e.getMessage(), false, null, null);
+                    } finally {
+                        appCMSUserDownloadVideoStatusCall.call(updateContentDatum.getGist().getId(), this,
+                                resultAction1, getLoggedInUser());
+                    }
                 });
             }
         });
