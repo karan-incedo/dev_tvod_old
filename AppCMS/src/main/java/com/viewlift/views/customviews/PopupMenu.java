7a1f95ef1ad1c5a0e214f9c9934307d776d030f1
wishy-mob-incedo <wishy.incedo@gmail.com>
2017-11-23 19:25:31 +0530

    team detail popup

--- AppCMS/src/main/java/com/viewlift/views/customviews/PopupMenu.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/PopupMenu.java
@@ -6,45 +6,49 @@ import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import com.viewlift.R;
 import com.viewlift.views.adapters.PopupMenuAdapter;
 
 import java.util.ArrayList;
 
-public class PopupMenu extends PopupWindow {
+import javax.inject.Inject;
 
+import butterknife.BindView;
+
+public class PopupMenu extends PopupWindow implements View.OnClickListener {
+
+    RelativeLayout ticketLayout;
+    RelativeLayout facebookLayout;
+    RelativeLayout instagramLayout;
     private Activity activity;
+    ListenerForPopMenu listenerForPopMenu;
+
     public PopupMenu(Activity activity) {
         super(activity);
+
         LayoutInflater inflater = (LayoutInflater) activity
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View popView = inflater.inflate(R.layout.pop_list, null);
         this.setContentView(popView);
-//		this.setWidth(dip2px(activity, 150));
-//        this.setHeight(LayoutParams.WRAP_CONTENT);
+//		this.setWidth(dip2px(activity, 200));
+//        this.setHeight(dip2px(activity, 200));
         this.setFocusable(true);
         this.setTouchable(true);
         this.setOutsideTouchable(true);
         ColorDrawable dw = new ColorDrawable(0x00000000);
         this.setBackgroundDrawable(dw);
-
-        ListView listView = (ListView) popView.findViewById(R.id.popList);
-        ArrayList<PopModel> popList = new ArrayList();
-        popList.add(new PopModel(R.drawable.ic_tickets, "Tickets"));
-        popList.add(new PopModel(R.drawable.ic_facebook, "Facebook"));
-        popList.add(new PopModel(R.drawable.ic_instagram, "Instagram"));
-        listView.setAdapter(new PopupMenuAdapter(popList));
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.e("pos",i+"");
-            }
-        });
+        ticketLayout = (RelativeLayout) popView.findViewById(R.id.popDialogLayout1);
+        facebookLayout = (RelativeLayout) popView.findViewById(R.id.popDialogLayout2);
+        instagramLayout = (RelativeLayout) popView.findViewById(R.id.popDialogLayout3);
+        ticketLayout.setOnClickListener(this);
+        facebookLayout.setOnClickListener(this);
+        instagramLayout.setOnClickListener(this);
     }
 
 
@@ -52,9 +56,35 @@ public class PopupMenu extends PopupWindow {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
-    public void showLocation(int resourId,Activity activity) {
+
+    public void showLocation(int resourId, Activity activity) {
         showAsDropDown(activity.findViewById(resourId), dip2px(activity, 0),
-                dip2px(activity, -8));
+                dip2px(activity, -20));
+    }
+
+    public void setListener(ListenerForPopMenu listenerForPopMenu) {
+        this.listenerForPopMenu = listenerForPopMenu;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == ticketLayout) {
+            listenerForPopMenu.ticketsClick();
+        }
+        if (view == facebookLayout) {
+            listenerForPopMenu.facebookClick();
+        }
+        if (view == instagramLayout) {
+            listenerForPopMenu.instagramClick();
+        }
+    }
+
+    public interface ListenerForPopMenu {
+        void ticketsClick();
+
+        void facebookClick();
+
+        void instagramClick();
     }
 
 }
