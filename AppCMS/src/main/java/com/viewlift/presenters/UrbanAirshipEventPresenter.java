562cd47bbff051b5a7e80b6caafca3a638e06ec0
jjhavstad <jonathan.havstad@outlook.com>
2017-12-18 12:28:43 -0500

    Added skeleton utility class for UA user churn.

---
+++ AppCMS/src/main/java/com/viewlift/presenters/UrbanAirshipEventPresenter.java
@@ -0,0 +1,27 @@
+package com.viewlift.presenters;
+
+import java.util.Date;
+
+/**
+ * Created by viewlift on 12/18/17.
+ */
+
+public class UrbanAirshipEventPresenter {
+
+    public void sendUserLoginEvent(String userId) {
+
+    }
+
+    public void sendUserLogoutEvent(String userId) {
+
+    }
+
+    public void sendSubscriptionStatusEvent(String userId, String subscriptionStatus) {
+
+    }
+
+    public boolean subscriptionAboutToExpire(Date subscriptionEndDate) {
+
+        return false;
+    }
+}
