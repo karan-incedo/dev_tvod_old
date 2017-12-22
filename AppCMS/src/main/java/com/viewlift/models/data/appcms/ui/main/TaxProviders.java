1422b275245edf9bb81b78e74baf34a94427c997
jjhavstad <jonathan.havstad@outlook.com>
2017-12-22 12:34:11 -0500

    Fixed positional parameter to use a boolean argument in the Shows API.

---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/ui/main/TaxProviders.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.data.appcms.ui.main;
+
+/**
+ * Created by viewlift on 12/22/17.
+ */
+
+class TaxProviders {
+}
---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/ui/page/Caching.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.data.appcms.ui.page;
+
+/**
+ * Created by viewlift on 12/22/17.
+ */
+
+class Caching {
+}
