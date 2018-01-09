5c60cb7805bf4243908b01ef42dcc964e19be421
jjhavstad <jonathan.havstad@outlook.com>
2018-01-09 18:03:20 -0500

    Refreshed app values to point ot Production and include updated UA User Churn tag groups.

--- AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
+++ AppCMS/src/main/java/com/viewlift/presenters/AppCMSPresenter.java
@@ -12143,8 +12143,8 @@ public class AppCMSPresenter {
                         .Builder()
                         .accessKey(UAirship.shared().getAirshipConfigOptions().getAppKey())
                         /** This value should ideally come from the Site.json response (2017-12-22 WIP AC-1384) */
-//                        .authKey("4qiw5pNUSuaw5HfAfVf-AQ") /** Production */
-                        .authKey("9NvLFbMITeuJtb-AqrwOpw") /** QA */
+                        .authKey("4qiw5pNUSuaw5HfAfVf-AQ") /** Production */
+//                        .authKey("9NvLFbMITeuJtb-AqrwOpw") /** QA */
                         .build();
     }
 
