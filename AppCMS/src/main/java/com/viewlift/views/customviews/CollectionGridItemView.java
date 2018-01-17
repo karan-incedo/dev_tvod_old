27f5a34d0749ab2ee6c82ce30c3c8f4dbf282d36
jjhavstad <jonathan.havstad@outlook.com>
2018-01-17 13:06:44 -0500

    Removed unneeded import.

--- AppCMS/src/main/java/com/viewlift/views/customviews/CollectionGridItemView.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/CollectionGridItemView.java
@@ -18,7 +18,6 @@ import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.StrikethroughSpan;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
