e2c48e3552ed50e4e3b859badee17acf5994b394
jjhavstad <jonathan.havstad@outlook.com>
2018-02-08 14:08:54 -0500

    Added log statements for API calls.

--- AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSPageAPICall.java
+++ AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSPageAPICall.java
@@ -106,8 +106,8 @@ public class AppCMSPageAPICall {
                 if (!TextUtils.isEmpty(authToken)) {
                     headersMap.put("Authorization", authToken);
                 }
-                Log.d(TAG, "API URL: " + urlWithContent);
-                Log.d(TAG, "API Headers: " + headersMap.toString());
+                Log.w(TAG, "API URL: " + urlWithContent);
+                Log.w(TAG, "API Headers: " + headersMap.toString());
                 Response<ResponseBody> response = null;
 
                 if (modules != null && !modules.isEmpty()) {
