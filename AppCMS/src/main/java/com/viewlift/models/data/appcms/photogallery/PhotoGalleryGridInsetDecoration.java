a3addcca097bc2328418cc3ca9c505116367b706
ramkailash-mob-incedo <ramkailash.incedo@gmail.com>
2018-02-16 18:39:12 +0530

    Crash Handling

--- AppCMS/src/main/java/com/viewlift/models/data/appcms/photogallery/PhotoGalleryGridInsetDecoration.java
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/photogallery/PhotoGalleryGridInsetDecoration.java
@@ -23,22 +23,26 @@ public class PhotoGalleryGridInsetDecoration extends RecyclerView.ItemDecoration
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
                                RecyclerView.State state) {
-        GridLayoutManager.LayoutParams layoutParams
-                = (GridLayoutManager.LayoutParams) view.getLayoutParams();
 
-        int position = layoutParams.getViewPosition();
-        if (position == RecyclerView.NO_POSITION) {
-            outRect.set(0, 0, 0, 0);
-            return;
+        if(view.getLayoutParams() instanceof GridLayoutManager.LayoutParams) {
+
+            GridLayoutManager.LayoutParams layoutParams
+                    = (GridLayoutManager.LayoutParams) view.getLayoutParams();
+
+            int position = layoutParams.getViewPosition();
+            if (position == RecyclerView.NO_POSITION) {
+                outRect.set(0, 0, 0, 0);
+                return;
+            }
+
+            // add edge margin only if item edge is not the grid edge
+            int itemSpanIndex = layoutParams.getSpanIndex();
+            // is left grid edge?
+            outRect.left = itemSpanIndex == 0 ? 0 : insetHorizontal;
+            // is top grid edge?
+            outRect.top = itemSpanIndex == position ? 0 : insetVertical;
+            outRect.right = 0;
+            outRect.bottom = 0;
         }
-
-        // add edge margin only if item edge is not the grid edge
-        int itemSpanIndex = layoutParams.getSpanIndex();
-        // is left grid edge?
-        outRect.left = itemSpanIndex == 0 ? 0 : insetHorizontal;
-        // is top grid edge?
-        outRect.top = itemSpanIndex == position ? 0 : insetVertical;
-        outRect.right = 0;
-        outRect.bottom = 0;
     }
 }
