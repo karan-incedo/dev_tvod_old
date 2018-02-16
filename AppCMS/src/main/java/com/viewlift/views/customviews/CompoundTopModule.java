5648915ea18a4bfe624427971b065d8fef1cb08c
sandeep <sandeep.singh@incedoinc.com>
2018-02-16 17:13:23 +0530

    Top Module code

--- AppCMS/src/main/java/com/viewlift/views/customviews/CompoundTopModule.java
+++ AppCMS/src/main/java/com/viewlift/views/customviews/CompoundTopModule.java
@@ -1,14 +1,10 @@
 package com.viewlift.views.customviews;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.viewlift.models.data.appcms.ui.AppCMSUIKeyType;
-import com.viewlift.models.data.appcms.ui.main.Content;
 import com.viewlift.models.data.appcms.ui.page.ModuleWithComponents;
 import com.viewlift.presenters.AppCMSPresenter;
 
@@ -31,7 +27,7 @@ public class CompoundTopModule extends ModuleView {
                              Map<String, AppCMSUIKeyType> jsonValueKeyMap,
                              AppCMSPresenter appCMSPresenter,
                              List<ModuleView> moduleViewList
-    ){
+    ) {
         super(context, moduleInfo, false);
 
         this.jsonValueKeyMap = jsonValueKeyMap;
@@ -40,26 +36,73 @@ public class CompoundTopModule extends ModuleView {
         this.mContext = context;
         init();
     }
-    public void init(){
+
+    public void init() {
         try {
             ViewGroup chieldContainer = getChildrenContainer();
-            LinearLayout topComponent = new LinearLayout(mContext);
+            if (BaseView.isTablet(mContext)) {
+                addComponentTab(chieldContainer);
 
-            if (!BaseView.isTablet(mContext)) {
-                topComponent.setOrientation(LinearLayout.VERTICAL);
+            } else {
+                addComponentMobile(chieldContainer);
             }
 
-            for (ModuleView moduleView:moduleViewList) {
-
-                topComponent.addView(moduleView);
-
-            }
-
-            chieldContainer.addView(topComponent);
-
-        }catch (Exception e){
-            System.out.println("======="+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("=======" + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private void addComponentMobile(final ViewGroup parentView) {
+        LinearLayout topComponent = new LinearLayout(mContext);
+        topComponent.setOrientation(LinearLayout.VERTICAL);
+
+        for (ModuleView moduleView : moduleViewList) {
+
+            topComponent.addView(moduleView);
+        }
+
+        parentView.addView(topComponent);
+    }
+
+    private void addComponentTab(final ViewGroup parent) {
+        LinearLayout topComponent = new LinearLayout(mContext);
+        topComponent.setWeightSum(100);
+
+        LinearLayout.LayoutParams crosualLP = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams topHeadlineLP = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        if (isLandscape(mContext)){
+            crosualLP.weight = 70;
+            topHeadlineLP.weight = 30;
+        }else {
+            crosualLP.weight = 60;
+            topHeadlineLP.weight = 40;
+        }
+        LinearLayout layoutHeadline =new LinearLayout(mContext);
+
+        topComponent.setOrientation(LinearLayout.HORIZONTAL);
+        layoutHeadline.setOrientation(LinearLayout.VERTICAL);
+
+        layoutHeadline.setLayoutParams(topHeadlineLP);
+
+
+        for (ModuleView moduleView : moduleViewList) {
+            switch (jsonValueKeyMap.get(moduleView.getModule().getType())) {
+                case PAGE_EVENT_CAROUSEL_MODULE_KEY:
+                    moduleView.setLayoutParams(crosualLP);
+                    topComponent.addView(moduleView);
+                    break;
+                case PAGE_LIST_MODULE_KEY:
+                    layoutHeadline.addView(moduleView);
+                    break;
+                case PAGE_MEDIAM_RECTANGLE_AD_MODULE_KEY:
+                    layoutHeadline.addView(moduleView);
+                    break;
+            }
+
+        }
+        topComponent.addView(layoutHeadline);
+        parent.addView(topComponent);
     }
 }
