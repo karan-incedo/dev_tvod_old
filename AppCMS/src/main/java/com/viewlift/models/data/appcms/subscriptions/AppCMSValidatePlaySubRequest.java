f51cb6c250fad74796969927028637901c40448e
jjhavstad <jonathan.havstad@outlook.com>
2018-02-22 17:44:59 -0500

    Added support for CC in the Chromecast extended controller.

---
+++ AppCMS/src/main/java/com/viewlift/models/data/appcms/subscriptions/AppCMSValidatePlaySubRequest.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.data.appcms.subscriptions;
+
+/**
+ * Created by viewlift on 2/22/18.
+ */
+
+public class AppCMSValidatePlaySubRequest {
+}
---
+++ AppCMS/src/main/java/com/viewlift/models/network/rest/AppCMSValidatePlaySubRest.java
@@ -0,0 +1,8 @@
+package com.viewlift.models.network.rest;
+
+/**
+ * Created by viewlift on 2/22/18.
+ */
+
+public interface AppCMSValidatePlaySubRest {
+}
