41280315c0abef619d31148b3ba296da27a9d8b5
wishy-mob-incedo <wishy.incedo@gmail.com>
2018-06-19 18:45:23 +0530

    plan 03 adapter

---
+++ AppCMS/src/main/java/com/viewlift/views/adapters/AppCMSPlansAdapter.java
@@ -0,0 +1,319 @@
+package com.viewlift.views.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.viewlift.Audio.playback.AudioPlaylistHelper;
+import com.viewlift.R;
+import com.viewlift.models.data.appcms.api.ContentDatum;
+import com.viewlift.models.data.appcms.api.Gist;
+import com.viewlift.models.data.appcms.api.Module;
+import com.viewlift.models.data.appcms.photogallery.IPhotoGallerySelectListener;
+import com.viewlift.models.data.appcms.ui.AppCMSUIKeyType;
+import com.viewlift.models.data.appcms.ui.android.AppCMSAndroidModules;
+import com.viewlift.models.data.appcms.ui.page.Component;
+import com.viewlift.models.data.appcms.ui.page.Layout;
+import com.viewlift.models.data.appcms.ui.page.Settings;
+import com.viewlift.presenters.AppCMSPresenter;
+import com.viewlift.views.customviews.BaseView;
+import com.viewlift.views.customviews.CollectionGridItemView;
+import com.viewlift.views.customviews.PhotoGalleryNextPreviousListener;
+import com.viewlift.views.customviews.ViewCreator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Created by viewlift on 5/5/17.
+ */
+
+public class AppCMSPlansAdapter extends RecyclerView.Adapter<AppCMSPlansAdapter.ViewHolder>
+        implements AppCMSBaseAdapter {
+    private static final String TAG = "AppCMSViewAdapter";
+    protected Context mContext;
+    protected Layout parentLayout;
+    protected Component component;
+    protected AppCMSPresenter appCMSPresenter;
+    protected Settings settings;
+    protected ViewCreator viewCreator;
+    protected Map<String, AppCMSUIKeyType> jsonValueKeyMap;
+    Module moduleAPI;
+    List<ContentDatum> adapterData;
+    CollectionGridItemView.OnClickHandler onClickHandler;
+    int defaultWidth;
+    int defaultHeight;
+    boolean useMarginsAsPercentages;
+    String componentViewType;
+    AppCMSAndroidModules appCMSAndroidModules;
+    private boolean useParentSize;
+    private AppCMSUIKeyType viewTypeKey;
+    private int unselectedColor;
+    private int selectedColor;
+    private boolean isClickable;
+    private String purchasePlanAction;
+    private MotionEvent lastTouchDownEvent;
+    private Gist preGist;
+
+    public AppCMSPlansAdapter(Context context,
+                              ViewCreator viewCreator,
+                              AppCMSPresenter appCMSPresenter,
+                              Settings settings,
+                              Layout parentLayout,
+                              boolean useParentSize,
+                              Component component,
+                              Map<String, AppCMSUIKeyType> jsonValueKeyMap,
+                              Module moduleAPI,
+                              int defaultWidth,
+                              int defaultHeight,
+                              String viewType,
+                              AppCMSAndroidModules appCMSAndroidModules) {
+        this.mContext = context;
+        this.viewCreator = viewCreator;
+        this.appCMSPresenter = appCMSPresenter;
+        this.parentLayout = parentLayout;
+        this.useParentSize = useParentSize;
+        this.component = component;
+        this.jsonValueKeyMap = jsonValueKeyMap;
+        this.moduleAPI = moduleAPI;
+        if (moduleAPI != null && moduleAPI.getContentData() != null) {
+            this.adapterData = moduleAPI.getContentData();
+        } else {
+            this.adapterData = new ArrayList<>();
+        }
+
+        this.componentViewType = viewType;
+        this.viewTypeKey = jsonValueKeyMap.get(componentViewType);
+        if (this.viewTypeKey == null) {
+            this.viewTypeKey = AppCMSUIKeyType.PAGE_EMPTY_KEY;
+        }
+
+
+        this.defaultWidth = defaultWidth;
+        this.defaultHeight = defaultHeight;
+        this.useMarginsAsPercentages = true;
+        this.purchasePlanAction = getPurchasePlanAction(context);
+
+        this.unselectedColor = ContextCompat.getColor(context, android.R.color.white);
+        this.selectedColor = Color.parseColor(appCMSPresenter.getAppCMSMain().getBrand()
+                .getCta().getPrimary().getBackgroundColor());
+        this.isClickable = true;
+
+        this.setHasStableIds(false);
+
+
+        this.appCMSAndroidModules = appCMSAndroidModules;
+
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CollectionGridItemView view = viewCreator.createCollectionGridItemView(parent.getContext(),
+                parentLayout,
+                useParentSize,
+                component,
+                appCMSPresenter,
+                moduleAPI,
+                appCMSAndroidModules,
+                settings,
+                jsonValueKeyMap,
+                defaultWidth,
+                defaultHeight,
+                useMarginsAsPercentages,
+                true,
+                this.componentViewType,
+                false,
+                useRoundedCorners(), this.viewTypeKey);
+
+        if (viewTypeKey == AppCMSUIKeyType.PAGE_SUBSCRIPTION_SELECTPLAN_03_KEY) {
+            applyBgColorToChildren(view, selectedColor);
+        }
+
+
+        return new ViewHolder(view);
+    }
+
+    private boolean useRoundedCorners() {
+        return mContext.getString(R.string.app_cms_page_subscription_selectionplan_03_key).equals(componentViewType);
+    }
+
+    private void applyBgColorToChildren(ViewGroup viewGroup, int bgColor) {
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            View child = viewGroup.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                if (child instanceof CardView) {
+                    ((CardView) child).setUseCompatPadding(true);
+                    ((CardView) child).setPreventCornerOverlap(false);
+                    ((CardView) child).setCardBackgroundColor(bgColor);
+                } else {
+                    child.setBackgroundColor(bgColor);
+                }
+                applyBgColorToChildren((ViewGroup) child, bgColor);
+            }
+        }
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        if (0 <= position && position < adapterData.size()) {
+            for (int i = 0; i < holder.componentView.getNumberOfChildren(); i++) {
+                if (holder.componentView.getChild(i) instanceof TextView) {
+                    ((TextView) holder.componentView.getChild(i)).setText("");
+                }
+            }
+
+            bindView(holder.componentView, adapterData.get(position), position);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (adapterData != null ? adapterData.size() : 0);
+    }
+
+    @Override
+    public void resetData(RecyclerView listView) {
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void updateData(RecyclerView listView, List<ContentDatum> contentData) {
+        listView.setAdapter(null);
+        adapterData = null;
+        notifyDataSetChanged();
+        adapterData = contentData;
+
+        //sortPlan(); as per MSEAN-1434
+
+        notifyDataSetChanged();
+        listView.setAdapter(this);
+        listView.invalidate();
+        notifyDataSetChanged();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    void bindView(CollectionGridItemView itemView,
+                  final ContentDatum data, int position) throws IllegalArgumentException {
+        if (onClickHandler == null) {
+            if (viewTypeKey == AppCMSUIKeyType.PAGE_SUBSCRIPTION_SELECTPLAN_03_KEY) {
+                onClickHandler = new CollectionGridItemView.OnClickHandler() {
+                    @Override
+                    public void click(CollectionGridItemView collectionGridItemView,
+                                      Component childComponent,
+                                      ContentDatum data, int clickPosition) {
+                        if (isClickable) {
+                            subcriptionPlanClick( data);
+                        }
+                    }
+
+                    @Override
+                    public void play(Component childComponent, ContentDatum data) {
+                        // NO-OP - Play is not implemented here
+                    }
+                };
+            }
+        }
+
+        for (int i = 0; i < itemView.getNumberOfChildren(); i++) {
+            itemView.bindChild(itemView.getContext(),
+                    itemView.getChild(i),
+                    data,
+                    jsonValueKeyMap,
+                    onClickHandler,
+                    componentViewType,
+                    appCMSPresenter.getBrandPrimaryCtaColor(),
+                    appCMSPresenter,
+                    position, null);
+        }
+    }
+
+    void subcriptionPlanClick(ContentDatum data) {
+        double price = data.getPlanDetails().get(0).getStrikeThroughPrice();
+        if (price == 0) {
+            price = data.getPlanDetails().get(0).getRecurringPaymentAmount();
+        }
+
+        double discountedPrice = data.getPlanDetails().get(0).getRecurringPaymentAmount();
+
+        boolean upgradesAvailable = false;
+        for (ContentDatum plan : adapterData) {
+            if (plan != null &&
+                    plan.getPlanDetails() != null &&
+                    !plan.getPlanDetails().isEmpty() &&
+                    ((plan.getPlanDetails().get(0).getStrikeThroughPrice() != 0 &&
+                            price < plan.getPlanDetails().get(0).getStrikeThroughPrice()) ||
+                            (plan.getPlanDetails().get(0).getRecurringPaymentAmount() != 0 &&
+                                    price < plan.getPlanDetails().get(0).getRecurringPaymentAmount()))) {
+                upgradesAvailable = true;
+            }
+        }
+
+        appCMSPresenter.initiateSignUpAndSubscription(data.getIdentifier(),
+                data.getId(),
+                data.getPlanDetails().get(0).getCountryCode(),
+                data.getName(),
+                price,
+                discountedPrice,
+                data.getPlanDetails().get(0).getRecurringPaymentCurrencyCode(),
+                data.getPlanDetails().get(0).getCountryCode(),
+                data.getRenewable(),
+                data.getRenewalCycleType(),
+                upgradesAvailable);
+
+    }
+
+    public boolean isClickable() {
+        return isClickable;
+    }
+
+    @Override
+    public void setClickable(boolean clickable) {
+        isClickable = clickable;
+    }
+
+    private String getPurchasePlanAction(Context context) {
+        return context.getString(R.string.app_cms_action_purchase_plan);
+    }
+
+    @Override
+    public void onViewRecycled(ViewHolder holder) {
+        super.onViewRecycled(holder);
+        int childCount = holder.componentView.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = holder.componentView.getChild(i);
+            if (child instanceof ImageView) {
+                Glide.with(child.getContext()).clear(child);
+            }
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CollectionGridItemView componentView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.componentView = (CollectionGridItemView) itemView;
+        }
+    }
+
+
+}
