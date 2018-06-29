1f34db02764a25c68c24da3220d4421f27d52c82
Anas Azeem <anas.azeem@incedoinc.com>
2018-06-29 13:55:41 +0530

    Sports Template Title cut-off issue Fixed

--- AppCMS/src/tv/java/com/viewlift/tv/views/customviews/TVBaseView.java
+++ AppCMS/src/tv/java/com/viewlift/tv/views/customviews/TVBaseView.java
@@ -301,7 +301,7 @@ public abstract class TVBaseView extends FrameLayout {
                     break;
                 case PAGE_VIDEO_TITLE_KEY:
                    // if (appCMSPresenter.getTemplateType().equals(AppCMSPresenter.TemplateType.ENTERTAINMENT)) {
-                        viewWidth = DEVICE_WIDTH/2 - Utils.getViewXAxisAsPerScreen(getContext() , 150);
+//                        viewWidth = DEVICE_WIDTH/2 - Utils.getViewXAxisAsPerScreen(getContext() , 150);
                    //   }
                     break;
                 case PAGE_VIDEO_SUBTITLE_KEY:
